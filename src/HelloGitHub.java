72e1c7f2d89021f5594e572582e3c11ce5fac5a1
QMOtt <QMOtt@10.200.58.106>
2017-10-31 15:16:28 -0400

    Quintin

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -3,7 +3,9 @@
  * @author smithk
  */
 public class HelloGitHub {
+    
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
+        System.out.println("Quintin");
     }
 }
